c736a6b458476c701a44480fd48fb913ec5c0a15
AlainDelombre <110764512+AlainDelombre@users.noreply.github.com>
2022-08-12 23:53:14 +0200

    Ajouts Chemins

---
+++ PetitChat.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="WA_zAlainPetitChat" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <image source="tilesets/PetitChat.png" width="32" height="32"/>
+</tileset>
